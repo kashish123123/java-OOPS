494f35307652cd8bcf64a86b5ee4fb0a25bec56d
kashish <kashish.271126@gmail.com>
2026-03-07 20:20:42 +0530

    UC1: completed OOPS print logic

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * Prints the literal text "OOPS" to the console.
+ * * @author KASHISH
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Method Invocation to print a String Literal
+        System.out.println("OOPS");
+    }
+}
