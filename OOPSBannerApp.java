afa52d0716ddede40bd3994b6ea9e6b659accdb4
unknown <kashish.271126@gmail.com>
2026-03-08 19:16:37 +0530

    UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,18 @@
-public class OOPSBannerApp {
-    public static void main(String[] args)
-    {
-        System.out.println("OOPS");
+/**
+ * UC2 - ASCII Banner for OOPS
+ * Prints OOPS using 7-line, 9-width grid format.
+ */
+public class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****  " + "  *****  " + " ********" + "  ****** ");
+        System.out.println(" **   ** " + " **   ** " + " **     *" + " **     *");
+        System.out.println("**     **" + "**     **" + " **     *" + " **       ");
+        System.out.println("**     **" + "**     **" + " ******** "+ " ****** ");
+        System.out.println("**     **" + "**     **" + " **       "+ "      **");
+        System.out.println(" **   ** " + " **   ** " + " **      " + "*     **");
+        System.out.println("  *****  " + "  *****  " + " **      " + " ******");
+
     }
 }
